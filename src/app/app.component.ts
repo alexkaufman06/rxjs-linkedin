4c1d436467ae46810b59a691032ae9467fe148b2
Kaufman <alexkaufman06@gmail.com>
2021-01-28 20:09:01 -0800

    Wrap API with observables

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit, OnDestroy } from '@angular/core';
-import {interval, of, fromEvent, Observable} from 'rxjs';
-import { take, map, filter, mergeMap, switchMap } from 'rxjs/operators';
+import {interval, of, fromEvent, Observable, Subject} from 'rxjs';
+import {take, map, filter, mergeMap, switchMap, debounceTime, distinctUntilChanged, tap} from 'rxjs/operators';
+import {HttpClient} from '@angular/common/http';
 
 @Component({
   selector: 'app-root',
@@ -8,10 +9,31 @@ import { take, map, filter, mergeMap, switchMap } from 'rxjs/operators';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
+  constructor(private http: HttpClient) {
+  }
   title = 'rxjs-linkedin';
+  searchSubject$ = new Subject<string>();
+  results$: Observable<any>;
 
   ngOnInit() {
-    fromEvent(document, 'click').subscribe(x => console.log(x));
+    this.results$ = this.searchSubject$.pipe(
+      debounceTime(200),
+      distinctUntilChanged(),
+      tap(x => console.log('do', x)),
+      switchMap(searchString => this.queryApi(searchString))
+    );
+  }
+
+  queryApi(searchString: string) {
+    console.log('queryAPI', searchString);
+    return this.http.get(`https://www.reddit.com/r/aww/search.json?q=${searchString}`).pipe(
+      map( (result: any) => result.data.children)
+    );
+  }
+
+  inputChanged($event) {
+    console.log('input changed: ', $event);
+    this.searchSubject$.next($event);
   }
 
   ngOnDestroy() {
