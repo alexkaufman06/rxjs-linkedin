72393f9182341ad78ab962708d7eecb0b2474eb8
Kaufman <alexkaufman06@gmail.com>
2021-01-28 19:40:54 -0800

    Add observable from click event

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit, OnDestroy } from '@angular/core';
-import { interval, of } from 'rxjs';
+import {interval, of, fromEvent, Observable} from 'rxjs';
 import { take, map, filter, mergeMap, switchMap } from 'rxjs/operators';
 
 @Component({
@@ -11,17 +11,7 @@ export class AppComponent {
   title = 'rxjs-linkedin';
 
   ngOnInit() {
-    const numbers$ = interval(1000);
-    const letters$ = of('a', 'b', 'c', 'd', 'e').pipe(
-      switchMap(x =>
-        numbers$.pipe(
-          take(5),
-          map(i => i + x)
-        )
-      )
-    );
-
-    letters$.subscribe(x => console.log(x));
+    fromEvent(document, 'click').subscribe(x => console.log(x));
   }
 
   ngOnDestroy() {
